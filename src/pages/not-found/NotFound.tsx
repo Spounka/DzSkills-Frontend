afcca968503f79d5ad62ae3e622705af5297089d
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-01 15:14:00 +0100

    simple 404 not found page

---
+++ src/pages/not-found/NotFound.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+
+function NotFound() {
+    return (
+        <Typography fontFamily={"Montserrat Arabic"} variant="h4" fontWeight={300}>404 Page Not found</Typography>
+    )
+}
+
+export default NotFound
