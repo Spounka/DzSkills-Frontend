ef011f51eac8fe593c55cc88ab427b889c2242bc
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-02 21:06:11 +0100

    edited MainButton component to have sx props

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -1,16 +1,17 @@
-import { Button, useTheme } from "@mui/material";
+import { Button, SxProps, useTheme } from "@mui/material";
 
 interface props {
     text: string,
     color?: string | undefined,
+    sx?: SxProps
 }
 
 
-export function MainButton({ text, color, ...other }: props) {
+export function MainButton({ text, color, sx, ...other }: props) {
     const theme = useTheme();
 
     return <Button size="large" variant="contained" sx={{
-        color: `${color || ""}`,
+        color: 'white',
         border: `${color || theme.palette.secondary.main} 2px solid`,
         alignSelf: 'center',
         px: 5,
@@ -18,9 +19,11 @@ export function MainButton({ text, color, ...other }: props) {
         transition: 'ease-in-out',
         transitionProperty: 'all',
         transitionDuration: '200ms',
+        ...sx,
         '&:hover': {
             bgcolor: 'white',
-            border: `${color} 2px solid`
+            border: `${color} 2px solid`,
+            color: `${color}`,
         }
 
     }}
