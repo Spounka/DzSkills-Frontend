499e8f6245b97b0487c5ff5370cf157c85d94bab
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-02 17:35:16 +0100

    Altered the button component to deal with color variations

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -1,15 +1,17 @@
-import { Button } from "@mui/material";
+import { Button, useTheme } from "@mui/material";
 
 interface props {
     text: string,
-    color: string,
+    color?: string | undefined,
 }
 
 
 export function MainButton({ text, color, ...other }: props) {
+    const theme = useTheme();
 
-    return <Button size="large" variant="contained" color={"secondary"} sx={{
-        border: `${color} 2px solid`,
+    return <Button size="large" variant="contained" sx={{
+        color: `${color || ""}`,
+        border: `${color || theme.palette.secondary.main} 2px solid`,
         alignSelf: 'center',
         px: 5,
         borderRadius: '.5rem',
@@ -18,7 +20,6 @@ export function MainButton({ text, color, ...other }: props) {
         transitionDuration: '200ms',
         '&:hover': {
             bgcolor: 'white',
-            color: { color },
             border: `${color} 2px solid`
         }
 
