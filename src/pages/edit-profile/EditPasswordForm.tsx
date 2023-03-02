cd394f29e720d8b8aa2a941f63da792f0cd5a9ea
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-02 23:19:10 +0100

    added EditPasswordForm sub section

---
+++ src/pages/edit-profile/EditPasswordForm.tsx
@@ -0,0 +1,40 @@
+import { ButtonGroup, useTheme } from '@mui/material';
+import Card from '@mui/material/Card';
+import { MainButton } from '../../components/ui/MainButton';
+import EditProfileField from './components/fields';
+import EditProfileColumn from './components/fields-column';
+
+export function EditPasswordForm({ }) {
+    const theme = useTheme();
+    return (<Card elevation={0} sx={{
+        gridColumnStart: 5,
+        gridColumnEnd: 14,
+        maxWidth: '100%',
+        py: theme.spacing(5),
+        px: theme.spacing(16),
+        borderRadius: theme.spacing(2),
+        display: 'flex',
+        flexDirection: "column",
+        gap: theme.spacing(4)
+    }}>
+
+        <EditProfileColumn>
+            <EditProfileField name={'current_password'} text={'كلمة السر الحالية'} type={'password'} />
+        </EditProfileColumn>
+        <EditProfileColumn>
+            <EditProfileField grow name={'password1'} text={'كلمة السر الجديدة'} type={'password'} />
+            <EditProfileField grow name={'password2'} text={'تأكيد كلمة السر'} type={'password'} />
+        </EditProfileColumn>
+        <ButtonGroup sx={{
+            transition: 'all ease-in-out 300ms',
+            gap: 4
+        }}>
+            <MainButton {...{
+                fullWidth: true
+            }} text={'حفظ'} color={theme.palette.primary.main} />
+            <MainButton {...{
+                fullWidth: true
+            }} text={'نبذ'} color={theme.palette.error.main} />
+        </ButtonGroup>
+    </Card>);
+}
