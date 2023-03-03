87512015a11778feb9e435ad4046311c53a9f797
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-03 11:00:02 +0100

    some changes on main button component

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -3,30 +3,34 @@ import { Button, SxProps, useTheme } from "@mui/material";
 interface props {
     text: string,
     color?: string | undefined,
-    sx?: SxProps
+    sx?: SxProps,
+    type?: "button" | 'reset' | 'submit';
 }
 
 
-export function MainButton({ text, color, sx, ...other }: props) {
+export function MainButton({ text, color, sx, type, ...other }: props) {
     const theme = useTheme();
 
-    return <Button size="large" variant="contained" sx={{
-        bgcolor: `${color || theme.palette.secondary.main}`,
-        color: 'white',
-        border: `${color || theme.palette.secondary.main} 2px solid`,
-        alignSelf: 'center',
-        px: 5,
-        borderRadius: '.5rem .5rem .5rem .5rem !important',
-        transition: 'ease-in-out',
-        transitionProperty: 'all',
-        transitionDuration: '200ms',
-        ...sx,
-        '&:hover': {
-            bgcolor: 'white',
-            border: `${color} 2px solid`,
-            color: `${color}`,
-        }
-    }}
+    return <Button size="large"
+        variant="contained"
+        type={type}
+        sx={{
+            bgcolor: `${color || theme.palette.secondary.main}`,
+            color: 'white',
+            border: `${color || theme.palette.secondary.main} 2px solid`,
+            alignSelf: 'center',
+            px: 5,
+            borderRadius: '.5rem .5rem .5rem .5rem !important',
+            transition: 'ease-in-out',
+            transitionProperty: 'all',
+            transitionDuration: '200ms',
+            ...sx,
+            '&:hover': {
+                bgcolor: 'white',
+                border: `${color} 2px solid`,
+                color: `${color}`,
+            }
+        }}
         {...other}
 
     >
