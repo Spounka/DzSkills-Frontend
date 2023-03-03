1940eda87179c0bb5313a0e5819179233e9bd78c
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-03 11:00:22 +0100

    Formatting fixes on IconFormPassword

--- src/components/form/IconFormPassword.tsx
+++ src/components/form/IconFormPassword.tsx
@@ -5,9 +5,6 @@ import { v4 as uuidv4 } from 'uuid';
 
 function IconFormPassword({ placeholder, ...other }: any) {
     const [showPassword, setShowPassword] = React.useState(false);
-
-
-
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
