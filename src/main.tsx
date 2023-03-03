18563d362ace53ba5407e7ddf0357fd196714aea
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-03 10:59:12 +0100

    Added React Query

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import theme from './theme'
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <BrowserRouter>
+                    <QueryClientProvider client={queryClient}>
                 <App />
+                    </QueryClientProvider>
             </BrowserRouter>
         </ThemeProvider>
     </React.StrictMode>,
