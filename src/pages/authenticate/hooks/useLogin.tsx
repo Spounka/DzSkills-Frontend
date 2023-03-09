8da4e045d423f3b535cc0ff4f9bb89c2217fc45e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-03-09 15:52:37 +0100

    added stale time and on success update state

--- src/pages/authenticate/hooks/useLogin.tsx
+++ src/pages/authenticate/hooks/useLogin.tsx
@@ -1,15 +1,19 @@
 import { useQuery } from 'react-query'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { updateUser } from '../../../redux/userSlice'
 import { getUser } from '../../edit-profile/api/getUser'
 
 function useLogin() {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     const token = localStorage.getItem('access_token')
     const refresh = localStorage.getItem('refresh_token')
     const query = useQuery({
+        staleTime: 1000 * 60 * 60,
         queryKey: ['user'],
         queryFn: () => getUser(token, refresh),
-        onSuccess: () => console.log('Success'), //navigate('/profile'),
+        onSuccess: (response) => dispatch(updateUser({ user: response })),
         onError: () => navigate('/login')
     })
     return [query]
