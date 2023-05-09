2353ea26df4c25c910d042b92ae475af34edf04d
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-05-09 21:06:31 +0100

    some colouring and stuff

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@mui/material/styles";
 import { LandingPageFirstSection } from "./LandingPageFirstSection";
 import { LandingPageNavbar } from "./LandingPageNavbar";
 import { LandingPageSections } from "./LandingPageSections";
@@ -8,8 +9,12 @@ interface LandingPageProps {
 }
 
 export default function LandingPage({ }: LandingPageProps) {
+    const theme = useTheme()
     return <div style={{
-        backgroundColor: '#F5F5F5'
+        backgroundColor: '#F5F5F5',
+        display: 'flex',
+        flexDirection: 'column',
+
     }}>
         <LandingPageNavbar />
         <LandingPageFirstSection />
