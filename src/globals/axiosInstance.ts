d1f26b7389eaad77231e1fbf756610011cd64bee
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-06-03 21:26:28 +0100

    some deployment configs

--- src/globals/axiosInstance.ts
+++ src/globals/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const token = localStorage.getItem('access_token');
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:80/api',
+    baseURL: import.meta.env.VITE_HOST || 'http://localhost:80/api',
     headers: {
         'Content-Type': 'multipart/form-data',
         Authorization: `Bearer ${token}`,
