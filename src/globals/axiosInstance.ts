123fe35b12ca463ebd3c05e754ec7c239264be74
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-06-04 00:14:11 +0100

    some random stuff for hosting

--- src/globals/axiosInstance.ts
+++ src/globals/axiosInstance.ts
@@ -3,10 +3,14 @@ import axios from 'axios';
 const token = localStorage.getItem('access_token');
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_HOST || 'http://localhost:80/api',
-    headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${token}`,
-    },
+    headers: token
+        ? {
+              'Content-Type': 'multipart/form-data',
+              Authorization: `Bearer ${token}`,
+          }
+        : {
+              'Content-Type': 'multipart/form-data',
+          },
 });
 
 export default axiosInstance;
