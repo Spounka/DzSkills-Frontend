bffcf4f84eae6c731f66b2bf085f8e4429359c69
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-08-12 07:17:38 +0100

    added teacher's side of MoneyRequest

--- src/pages/teacher-dashboard/landing-page/Reminders.tsx
+++ src/pages/teacher-dashboard/landing-page/Reminders.tsx
@@ -1,13 +1,48 @@
 import { ChatBubbleOutlineOutlined } from '@mui/icons-material';
-import { Badge, Box, Divider, Stack, Typography, useTheme } from '@mui/material';
+import {
+    Badge,
+    Box,
+    CircularProgress,
+    Divider,
+    Stack,
+    Typography,
+    useTheme,
+} from '@mui/material';
+import { enqueueSnackbar } from 'notistack';
+import { useMutation } from 'react-query';
 import { ReactComponent as MoneyGreen } from '../../../assets/svg/money-nocolor.svg';
 import { ReactComponent as TimeOrange } from '../../../assets/svg/time-orange.svg';
 import { ReactComponent as TimePurple } from '../../../assets/svg/time-purple.svg';
 import { StyledCard } from '../../../components/StyledCard';
+import axiosInstance from '../../../globals/axiosInstance';
+import { MoneyRequest } from '../../../types/account-balance';
 import { ReminderItem } from './ReminderItem';
 
-export function Reminders({}) {
+interface Props {
+    balance: number;
+}
+
+export function Reminders({ balance }: Props) {
     const theme = useTheme();
+    const submitPaymentMutation = useMutation({
+        mutationKey: ['submitAccount'],
+        mutationFn: async () => {
+            if (balance < 1000) {
+                enqueueSnackbar('رصيد حساب أقل من 1000 دج', { variant: 'error' });
+                return Promise.reject('balance too small');
+            }
+            const { data } = await axiosInstance.post('/balance/request/', {
+                amount: balance,
+            });
+            return data as MoneyRequest;
+        },
+        onSuccess: () => {
+            enqueueSnackbar('تم إرسال طلب الأموال بنجاح', { variant: 'success' });
+        },
+        onError: () => {
+            enqueueSnackbar('فشل طلب سحب الأموال', { variant: 'error' });
+        },
+    });
     return (
         <StyledCard
             sx={{
@@ -54,9 +89,10 @@ export function Reminders({}) {
                     }
                     color={'secondary.main'}
                     subtitle="اطلع على الرسائل التي لم تقرأها بعد"
-                />{' '}
+                />
                 <ReminderItem
                     title={'تأكيد دفع'}
+                    onClick={() => submitPaymentMutation.mutate()}
                     Icon={
                         <Box
                             sx={{
@@ -65,13 +101,24 @@ export function Reminders({}) {
                                 p: 1.5,
                             }}
                         >
-                            <MoneyGreen
-                                style={{
-                                    fill: theme.palette.primary.main,
-                                    width: '100%',
-                                    height: '100%',
-                                }}
-                            />
+                            {submitPaymentMutation.isLoading ? (
+                                <CircularProgress
+                                    //@ts-ignore
+                                    color="purple"
+                                    sx={{
+                                        width: '100%',
+                                        height: '100%',
+                                    }}
+                                />
+                            ) : (
+                                <MoneyGreen
+                                    style={{
+                                        fill: theme.palette.primary.main,
+                                        width: '100%',
+                                        height: '100%',
+                                    }}
+                                />
+                            )}
                         </Box>
                     }
                     color={'primary.main'}
