a7c450da9a5c3f354a715f2f6622cbb4cc014059
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-08-14 22:14:41 +0100

    added a fullscreen loading fallback component

---
+++ src/components/full-screen-loading-fallback.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
+function FullScreenLoadingFallback() {
+    return (
+        <div style={{
+            width: '100vw',
+            height: '100dvh',
+            margin: 0,
+            padding: 0,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 16,
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <CircularProgress color={'primary'} />
+            <h3 style={{
+                fontFamily: 'Montserrat Arabic',
+
+            }}>
+                يرجى الانتظار حتى يتم تحميل الموقع
+            </h3>
+        </div>
+    );
+}
+
+export default FullScreenLoadingFallback;
--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { ComponentPreview, Previews } from '@react-buddy/ide-toolbox';
 import {PaletteTree} from "./palette";
-import FullPageLoading from '../components/full-page-loading';
+import FullScreenLoadingFallback from '../components/full-screen-loading-fallback';
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path='/FullPageLoading'>
-                <FullPageLoading />
+                <FullScreenLoadingFallback />
             </ComponentPreview>
         </Previews>
     );
